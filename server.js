de3d1fecb9cb03ebee28905790621cee52615c25
Anton Galanov <Anton_Galanov@epam.com>
2021-02-09 18:25:23 +0300

    Fix origin headers

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function parseEnvList(env) {
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 //var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
-var checkRateLimit = require('./lib/rate-limit')('50 3');
+var checkRateLimit = require('./lib/rate-limit')('500 3');
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
